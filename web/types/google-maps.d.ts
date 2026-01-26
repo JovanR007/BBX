d57473cef28737908b0f38603450fec9cef55184
Jovan Rodriguez <shearjovan7@gmail.com>
2026-01-26 10:49:55 +0800

    fix: simplify google maps d.ts to global namespace

--- web/types/google-maps.d.ts
+++ web/types/google-maps.d.ts
@@ -1,16 +1,14 @@
-import React from 'react';
+/// <reference types="react" />
 
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'gmpx-api-loader': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-                key?: string;
-                'solution-channel'?: string;
-            };
-            'gmpx-place-picker': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-                placeholder?: string;
-                ref?: React.RefObject<any>;
-            };
-        }
+declare namespace JSX {
+    interface IntrinsicElements {
+        "gmpx-api-loader": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            key?: string;
+            "solution-channel"?: string;
+        };
+        "gmpx-place-picker": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            placeholder?: string;
+            ref?: React.RefObject<any>;
+        };
     }
 }
